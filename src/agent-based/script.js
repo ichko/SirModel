255a37ceb4ab8b0f9b6fdc6d672e78f448b02b18
Iliya Zhechev <i4ko95@gmail.com>
2016-04-16 00:49:59 +0300

    Agar cleanup

--- src/agent-based/script.js
+++ src/agent-based/script.js
@@ -1,3 +1,52 @@
+var ModelHelper = (function(){
+    var ModelHelper = {};
+
+    ModelHelper.getNeightbours = function(agent, agents){
+        var neighbours = [];
+        for (var i = 0; i < agents.length; i++) {
+            if(agent.pos.distance(agents[i].pos) < this.distance){
+                neighbours.push(agents[i]);
+            }
+        }
+        return neighbours;
+    }
+
+    return ModelHelper;
+})();
+
+
+var AgarModel = (function(){
+    function AgarModel(distance){
+        this.distance = distance || 10;
+    }
+
+    AgarModel.prototype.mutateAgent = function(agent, world){
+        agent.move(world.size);
+        var agents = world.agents;
+
+        for (var i = 0; i < agents.length; i++) {
+            if(agent.pos.distance(agents[i].pos) < agent.size + agents[i].size &&
+                    agent.size > agents[i].size &&
+                    agents[i].desiredSize != 0){
+                agent.desiredSize += agents[i].size / 2;
+                agents[i].desiredSize = 0;
+            }
+            if(agents[i].size < 0.01){
+                agents.splice(i, 1);
+            }
+        }
+    }
+
+    return AgarModel;
+})();
+
+
+
+/////////////////////////////////
+/////////////////////////////////
+
+
+
 Number.prototype.mod = function(n) {
     return ((this % n) + n) % n;
 };
